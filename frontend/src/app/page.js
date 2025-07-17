1b2699fc2c3f94721fe45f74505361d261ad00e4
Phantem717 <pradahsuherli@gmail.com>
2025-07-17 15:32:23 +0700

    PACKAGE UPDATE

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 
 export default function Home() {
   useEffect(() => {
-    window.location.href = "http://172.16.21.214:3009/login"; // 🔀 Redirect langsung ke login RBAC
+    window.location.href = "http://192.168.6.106/login"; // 🔀 Redirect langsung ke login RBAC
   }, []);
 
   return null; // Tidak perlu menampilkan apapun karena langsung redirect
