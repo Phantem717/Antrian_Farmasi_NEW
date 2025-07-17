5b6c19f5eab38f1d88873befde36b02daea35f3e
Phantem717 <pradahsuherli@gmail.com>
2025-07-17 15:09:58 +0700

    CHANGED DB

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,11 +1,12 @@
 // src/config/db.js
 const mysql = require('mysql2/promise');
+    require('dotenv').config();
 
 const dbConfig = {
-  host: 'localhost',
-  user: 'root',      // ganti dengan username MySQL kamu
-  password: '',      // ganti dengan password MySQL kamu
-  database: 'farmasi_queue_db'   // nama database yang ingin digunakan
+host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME, // nama database yang ingin digunakan
 };
 
 let connection;
