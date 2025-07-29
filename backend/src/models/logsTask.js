b5a983c79b3f4fb575cb5c5d3eff6a19f0c32e90
Phantem717 <pradahsuherli@gmail.com>
2025-07-29 10:40:07 +0700

    LOGS UPDATED

--- backend/src/models/logsTask.js
+++ backend/src/models/logsTask.js
@@ -324,8 +324,42 @@ AND pt.lokasi = ?
    
   }
 
-  static async getAvgServiceTime(location,fromDate,toDate){
-  const pool = await getDb();
+   static async getAvgServiceTime(location){
+    const pool = await getDb();
+  const conn = await pool.getConnection(); // ? Explicit connection
+
+    try {
+      const query = `SELECT 
+    AVG(CASE WHEN da.status_medicine = 'Racikan' 
+             THEN TIMESTAMPDIFF(MINUTE, vt.waiting_verification_stamp, pa.completed_pickup_medicine_stamp) 
+             ELSE NULL END) AS 'AVG PROCESSING TIME - RACIKAN (MINUTES)',
+    AVG(CASE WHEN da.status_medicine != 'Racikan' OR da.status_medicine IS NULL
+             THEN TIMESTAMPDIFF(MINUTE, vt.waiting_verification_stamp, pa.completed_pickup_medicine_stamp) 
+             ELSE NULL END) AS 'AVG PROCESSING TIME - NON-RACIKAN (MINUTES)'
+    
+FROM Doctor_Appointments da
+LEFT JOIN Verification_Task vt ON da.NOP = vt.NOP
+LEFT JOIN Pharmacy_Task pt ON da.NOP = pt.NOP
+LEFT JOIN Medicine_Task mt ON da.NOP = mt.NOP
+LEFT JOIN Pickup_Task pa ON da.NOP = pa.NOP
+WHERE pt.status = 'completed_pickup_medicine'
+AND pt.lokasi = ?
+  `;
+  
+  const [rows] = await conn.execute(query,[location]);
+  return rows;
+    } catch (error) {
+      return error;
+    }finally {
+    conn.release(); // ?? Critical cleanup
+  }
+   
+   
+
+  }
+
+   static async getAvgServiceTimeByDate(fromDate,toDate,location){
+    const pool = await getDb();
   const conn = await pool.getConnection(); // ? Explicit connection
 
     try {
@@ -351,7 +385,8 @@ AND (Date(vt.waiting_verification_stamp) BETWEEN ? AND ?)
   return rows;
     } catch (error) {
       return error;
-    }finally {
+    }
+   finally {
     conn.release(); // ?? Critical cleanup
   }
    
