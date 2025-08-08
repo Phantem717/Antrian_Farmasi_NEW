fa56a913bccefd9c27faf4c6f00d4b168c6dc530
Phantem717 <pradahsuherli@gmail.com>
2025-08-08 07:08:34 +0700

    marquee

--- frontend/src/app/component/display/bpjs/NextQueue_miss.js
+++ frontend/src/app/component/display/bpjs/NextQueue_miss.js
@@ -33,8 +33,8 @@ const socket = getSocket();
   function calculateTime(processLengthNon, processLengthRacik, pickupLengthNon, pickupLengthRacik) {
   const processTimeNon = processLengthNon < 3 ? 10 : (Math.floor(processLengthNon * 1.5));
   const processTimeRacik =processLengthRacik< 3 ? 10 : (Math.floor(processLengthRacik * 0.16));
-  const pickupTimeNon = pickupLengthNon < 3 ? 10 : (pickupLengthNon * 10);
-  const pickupTimeRacik = pickupLengthRacik < 3 ? 10 : (pickupLengthRacik * 10);
+  const pickupTimeNon = pickupLengthNon < 3 ? 10 : (pickupLengthNon * 15);
+  const pickupTimeRacik = pickupLengthRacik < 3 ? 10 : (pickupLengthRacik * 1.5);
   return {  processTimeNon,processTimeRacik, pickupTimeNon,pickupTimeRacik };
 }
 
